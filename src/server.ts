b80812ba010e2dfa6828ca91513f79de94c5c3ca
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-07 20:05:30 -0700

    try again hombre

--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,25 @@ import express from 'express';
 import cors from "cors";
 
 const fs = require('fs');
+const path = require('path');
 const serverless = require('serverless-http');
 
 // Declare Express App
 const app = express();
+const router = express.Router();
 
-app.use(cors());
-
-app.get("/results", (req, res) => {
+router.get("/results", (req, res) => {
   const rawData = fs.readFileSync(`${__dirname}/similarityResults.json`);
-  res.send({ results: JSON.parse(rawData) });
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.write({ results: JSON.parse(rawData) });
+  res.end();
   console.log("We did it bois");
 });
+
+
+app.use('/results', (req, res) => res.sendFile(path.join(__dirname, '/similarityResults.json')));
+
+app.use(cors());
 
 module.exports = app;
 module.exports.handler = serverless(app);
