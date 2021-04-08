c66925df0e1e413935961c030f03e47bf23a60a4
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-07 20:12:12 -0700

    update stuff

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,6 @@ const serverless = require('serverless-http');
 
 // Declare Express App
 const app = express();
-const router = express.Router();
-
-router.get("/results", (req, res) => {
-  const rawData = fs.readFileSync(`${__dirname}/similarityResults.json`);
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.write({ results: JSON.parse(rawData) });
-  res.end();
-  console.log("We did it bois");
-});
-
 
 app.use('/results', (req, res) => res.sendFile(path.join(__dirname, '/similarityResults.json')));
 
