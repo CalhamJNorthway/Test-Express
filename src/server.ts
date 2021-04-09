65d9a8911dfb1c92bbbbd310489872c511562564
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-09 10:03:25 -0700

    Test Update

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,21 @@ const fs = require('fs');
 const path = require('path');
 const serverless = require('serverless-http');
 
+
 // Declare Express App
 const app = express();
-
+app.use(cors());
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 app.get('/help', (req, res) => res.send({ "Message": "HELLO" }));
 
 app.use('/results', (req, res) => res.sendFile(path.join(__dirname, '/similarityResults.json')));
 
-app.use(cors());
+app.use('/.netlify/functions/server', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
