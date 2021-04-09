6c9429ac053e010f32f7daddd81ce84d005bb231
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-09 10:19:52 -0700

    Test Update

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,7 @@ app.get('/help', (req, res) => res.send({ "Message": "HELLO" }));
 app.use('/results', (req, res) => res.sendFile(path.join(__dirname, '/similarityResults.json')));
 
 app.use('/functions/server', router);
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
